69413fd61569a63d2d2d4188eadb8be83cf6656c
Timmenem <nuffnuff@gmx.de>
2017-06-20 15:12:50 +0200

    Delete unnecessary files

--- Drivers/__init__.py
+++
@@ -1,5 +0,0 @@
-__all__ = ['Driver', 'sensor']
-class Driver:
-    def dummyfunction():
-        print("Hello World")
-        return False
--- MulticastSender.py
+++
@@ -1,9 +0,0 @@
-class MulticastSender:
-    """
-    Simple class to do multicast announcements and searches
-    """
-    def __init__(self):
-        pass
-
-    def send(self, dict_to_send):
-        pass
