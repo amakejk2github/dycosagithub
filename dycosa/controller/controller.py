905dd3a7f8fdc737d354b6fc45682f4419e45514
Jtlatik <Jtlatik@upb.de>
2017-07-17 22:54:43 +0200

    Implemented further search/find methods

--- dycosa/controller/controller.py
+++ dycosa/controller/controller.py
@@ -25,8 +25,12 @@ class Controller:
         self.drivers = self.load_drivers()
         self.multicast_sender = MulticastSender()
         self.multicast_receiver = MulticastReceiver()
-        print(self.get_config("Text"))#Test function
-#       self.job_controller = JobController()
+        print(self.get_config("Type"))  # Test function
+        print(self.get_config("Sleep"))  # Test function
+        print(self.get_config("Text"))  # Test function
+        print(self.get_config("Permanent_power"))  # Test function
+
+    #       self.job_controller = JobController()
 
     def load_config(self):
         # Opens file and converts JSON object to strings, dicts, etc.
@@ -36,15 +40,44 @@ class Controller:
 
     def create_dict_for_config(self, JSON_data, path, last_key):
         try:
-            keys = JSON_data.items()
             for key, value in JSON_data.items():
-                if key != "Settable" and key != "Visible":
-                    self.create_dict_for_config(JSON_data[key], path + "/" + key, key)  # TODO "Value" kann man nicht ordentlich wiederfinden FIXEN
+                if key == 'Value':
+                    self.config_path_dict[last_key] = path
+                if key != 'Settable' and key != 'Visible' and key != 'Value':
+                    self.create_dict_for_config(JSON_data[key], path + "/" + key, key)
         except:
             self.config_path_dict[last_key] = path
 
     def get_config(self, attribute):
-        path = self.config_path_dict[attribute]
+        data = self.find_config_entry(attribute)
+        visible_value = True
+        try:
+            visible_value = data['Visible']
+        except:
+            pass
+        if visible_value:
+            try:
+                return data['Value']
+            except:
+                return data
+        else:
+            raise Exception(f"Attribute {attribute} is not visible")
+
+    def set_config(self, attribute, entry):
+        data = self.find_config_entry(attribute)
+        settable_value = True
+        try:
+            settable_value = data['Settable']
+        except:
+            pass
+
+
+
+    def find_config_entry(self, attribute):
+        try:
+            path = self.config_path_dict[attribute]
+        except:
+            raise Exception(f"Attribute {attribute} is not available")
         list_of_path = path.split('/')
         data = self.config_data
         for pathpart in list_of_path:
@@ -63,16 +96,13 @@ class Controller:
                     if (cls is not Driver and issubclass(cls, Driver)):
                         clsobj = cls()
                         if not hasattr(clsobj, "endpoint"):
-                            raise Exception("Driver {driver} does not implement the endpoint property".format(
-                                driver=clsobj.__name__))
+                            raise Exception(f"Driver {clsobj.__name__} does not implement the endpoint property")
                         endpoint = clsobj.endpoint
                         i = 0
                         while (endpoint + str(i)) in drivers:
                             i = i + 1
                         drivers[endpoint + str(i)] = clsobj
-                        print("Loaded driver of type {type} with endpoint {endpoint}".format(type=clsobj.__name__,
-                                                                                             endpoint=endpoint + str(
-                                                                                                 i)))
+                        print(f"Loaded driver of type {clsobj.__name__} with endpoint {endpoint + str(i)}")
                 except:
                     pass
         return drivers
