148c7618676d23377f53f91fe8e16f632293cf40
Jtlatlik <Jtlatlik@upb.de>
2017-07-19 15:26:56 +0200

    Transfer all functions to config_controller

--- dycosa/controller/config_controller.py
+++ dycosa/controller/config_controller.py
@@ -21,22 +21,31 @@ class Config_Controller:
         return self.config_data
 
     def write_config_to_data(self):
-        with open('Node_config.json', 'w') as OutputFile:
-            json.dump(self.config_data, OutputFile, indent=4, sort_keys=False)
+        try:
+            with open('Node_config.json', 'w') as OutputFile:
+                json.dump(self.config_data, OutputFile, indent=4, sort_keys=False)
+        except:
+            raise Exception(f"Write to config file failed")
 
-    def set_config(self, attribute, entry, write_to_file):
-        data = self.find_config_entry(attribute)
+    def set_config(self, list, entry):
+        data = self.config_data
+        attribute_name = ""
+        for key in list:
+            if key in data.keys():
+                data = data[key]
+                attribute_name = key
+            else:
+                raise Exception(f"Key {key} is not in the config")
         settable_value = True
         if 'Settable' in data.keys():
             settable_value = data['Settable']
         if not settable_value:
-            raise Exception(f"Attribute {attribute} is not settable")
+            raise Exception(f"Attribute {attribute_name} is not settable")
         try:
             if 'Value' in data.keys():
                 data["Value"] = entry
             else:
-                data[attribute] = entry
+                data[attribute_name] = entry
         except:
-            raise Exception (f"Fatal error occurred, while writing {attribute}")
-        if write_to_file:
-            self.write_config_to_data()
+            raise Exception (f"Fatal error occurred, while writing {attribute_name}")
+        self.write_config_to_data()
