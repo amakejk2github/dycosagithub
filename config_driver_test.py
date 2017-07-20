31d173974b3c7c817ce4f6ed5d0cd90f0b9845cf
Jtlatlik <Jtlatlik@upb.de>
2017-07-20 06:30:34 +0200

    Add Config_driver tests

---
+++ config_driver_test.py
@@ -0,0 +1,24 @@
+from dycosa.drivers.config_driver import Config_Driver
+import pytest
+import json
+
+@pytest.fixture()
+def prefab():
+    return Config_Driver()
+
+def test_get_function(prefab):
+    with open('Node_Config.json', encoding='utf-8') as node_config:
+        config_data = json.loads(node_config.read())
+    assert prefab.get_config() == config_data
+
+def test_set_function(prefab):
+    prefab.set_config(["Device", "Sleep"], 0)
+    with open('Node_Config.json', encoding='utf-8') as node_config:
+        config_data = json.loads(node_config.read())
+    assert prefab.get_config() == config_data
+
+def test_set_function2(prefab):
+    with pytest.raises(Exception):
+        prefab.set_config(["Device", "Location", "Text"], "P7.2.03")
+
+
