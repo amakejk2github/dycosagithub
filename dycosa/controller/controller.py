4fbed9d1144636088de40da03ec7c3a9624d1959
Jtlatlik <Jtlatlik@upb.de>
2017-07-31 11:17:46 +0200

    Replace load_drivers() function with load_driver_from_config() function to load only the drivers from the config

--- dycosa/controller/controller.py
+++ dycosa/controller/controller.py
@@ -2,6 +2,7 @@ from dycosa.drivers import *
 from dycosa.controller.rest_api import RestApi
 from dycosa.controller.multicast_sender import MulticastSender
 from dycosa.controller.multicast_receiver import MulticastReceiver
+from dycosa.drivers.config_driver import Config_Driver
 # from dycosa.job.job_controller import JobController
 
 import json
@@ -18,32 +19,48 @@ class Controller:
     """
 
     def __init__(self):
-        self.drivers = self.load_drivers()
+        self.config = Config_Driver()
+        self.drivers = self.load_driver_from_config()
         self.multicast_sender = MulticastSender()
         self.multicast_receiver = MulticastReceiver()
         #self.job_controller = JobController()
 
-    def load_drivers(self):
-        drivers = dict()
-        global_objs = list(globals().items())
-        for name, obj in global_objs:
-            for subObj in dir(obj):
 
-                cls = getattr(obj, subObj)
+    def load_driver_from_config(self):
+        global_objs = list(globals().items())
+        drivers = dict()
+        loadable_drivers = dict()
+        for name, object in global_objs:
+            for subObj in dir(object):
+                cls = getattr(object, subObj)
                 try:
-                    if (cls is not Driver and issubclass(cls, Driver)):
-                        clsobj = cls()
-                        if not hasattr(clsobj, "endpoint"):
-                            raise Exception("Driver {name} does not implement the endpoint property".format(name = clsobj.__name__))
-                        endpoint = clsobj.endpoint
-                        i = 0
-                        while (endpoint + str(i)) in drivers:
-                            i = i + 1
-                        drivers[endpoint + str(i)] = clsobj
-                        print("Loaded driver of type {name} with endpoint {end}".format(name = clsobj.__name__, end = endpoint + str(i)))
+                    if(cls is not Driver and issubclass(cls, Driver)):
+                        loadable_drivers[name] = cls
                 except:
                     pass
+        data = self.config.get_config()
+        data = data["Drivers"]
+        for key in data.keys():
+            driver_data = data[key]
+            class_name = driver_data["InstanceOf"]
+            if not class_name in loadable_drivers.keys():
+                raise Exception ("Driver {name} is not implemented or wrong name given".format(name = class_name))
+            classobj = loadable_drivers[class_name]()
+            #TODO implement drivers to accept comments and internal configs as parameters, then loadable_drivers[class_name](driver_data["Comment], driver_data["Config"])
+            if not hasattr(classobj, "endpoint"):
+                raise Exception("Driver {name} does not implement the endpoint property".format(name=classobj.__name__))
+            endpoint = classobj.endpoint
+            i = 0
+            while (endpoint + str(i)) in drivers:
+                i = i + 1
+            drivers[endpoint + str(i)] = classobj
+            print("Loaded driver of type {name} with endpoint {end}".format(name=classobj.__name__, end=endpoint + str(i)))
         return drivers
+
+
+
+
+
 
     def run(self):
         api = RestApi(self.drivers)
