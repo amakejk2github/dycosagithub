15801388542502ce1ec0c7c78aa4477c3ef897f9
PMI1617AM <amakejk2@campus.uni-paderborn.de>
2017-08-01 18:08:23 +0200

    test

---
+++ dycosa/controller/mutlicast_receiver.py
@@ -0,0 +1,11 @@
+class MulticastReceiver:
+    """
+    Simple class to recieving multicast announcements and searches
+    If necessary it also reacts to those messages
+    """
+    
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
